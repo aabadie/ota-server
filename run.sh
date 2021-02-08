9fabefac58310bef4b08bc960ca355cddd3177d3
Francisco Molina <femolina@uc.cl>
2021-02-08 14:56:49 +0100

    Dockerfile: allow specifying HTTP port and host

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 
 : ${COAP_HOST:=[::1]}
 : ${COAP_PORT:=5683}
+: ${HTTP_HOST:=localhost}
+: ${HTTP_PORT:=8080}
 : ${ROOT_URL:=}
 
 python3 /opt/ota-server/otaserver/main.py \
             --coap-host=${COAP_HOST} --coap-port=${COAP_PORT} \
+            --http-host=${HTTP_HOST} --http-port=${HTTP_PORT} \
             --root-url=${ROOT_URL} --debug
